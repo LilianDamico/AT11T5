df1bd91e6babe0fe8e5444a1cb0f51b666c85af9
lfons <lfons@DESKTOP-8PJVQIC>
2022-11-08 09:45:42 -0300

    Adicionar arquivos de projeto.

---
+++ AT11T5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Inicio
+int n = 0;
+int cont = 0;
+int i = 0;
+while (cont < 10)
+{
+    Console.Write("Informe um valor: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n < 0)
+    {
+        i++;
+    }
+    cont++;
+}
+Console.WriteLine("Foram informados " + i + " valores negativos");
+Console.ReadKey();
+//Fim
